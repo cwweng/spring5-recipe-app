5a207636380f09a13eca65353071b1f770b2c2b5
Isaac Weng <cwweng@gmail.com>
2018-03-19 15:15:16 +0800

    Add One to One relation from Ingredient to UnitOfMeasure

--- src/main/java/net/cwweng/spring5recipeapp/models/Ingredient.java
+++ src/main/java/net/cwweng/spring5recipeapp/models/Ingredient.java
@@ -11,6 +11,9 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
     @ManyToOne
     private Recipe recipe;
 
